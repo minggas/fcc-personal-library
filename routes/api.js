7d10c9c57b5d402a854b1d57f801969fdc55180a
minggas <fasfora@gmail.com>
2019-07-16 01:53:36 -0300

    feat: change some es5 to es6
    - var to const or let
    - arrow functions

--- routes/api.js
+++ routes/api.js
@@ -1,96 +1,105 @@
 /*
-*
-*
-*       Complete the API routing below
-*       
-*       
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+const mongoose = require("mongoose");
+const collection = process.env.DB + "books";
+const Book = require("../models/Book").Book;
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId;
-var mongoose = require('mongoose');
-var collection = 'test';
-var Book = require('../models/Book').Book;
+mongoose.connect(collection + "?retryWrites=true", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-mongoose.connect(process.env.DB + collection + "?retryWrites=true", {useNewUrlParser:true});
-
-module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+module.exports = function(app) {
+  app
+    .route("/api/books")
+    .get((req, res) => {
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-    
-      Book.find({},(err, doc) => {
-        if(err) console.log(err);
+
+      Book.find({}, (err, doc) => {
+        if (err) console.log(err);
         const result = doc.map(book => {
-          const {_id, title, comments} = book;
+          const { _id, title, comments } = book;
           const commentcount = comments.length;
-          return {_id, title, commentcount}
-        })  
-        res.status(200).json(result);        
-      })
+          return { _id, title, commentcount };
+        });
+        res.status(200).json(result);
+      });
     })
-    
-    .post(function (req, res){
-      if(!req.body.title) {
-        res.status(400).send('Please enter title');
+
+    .post((req, res) => {
+      if (!req.body.title) {
+        res.status(400).send("Please enter title");
       } else {
-        var title = req.body.title;
-        var newBook = new Book({title});
-        newBook.save().then(result => {
-          var {title, _id} = result;
-          res.status(200).json({title, _id});
-        }).catch(err => {
-          res.status(400).json(err);
-        })    
-      }        
+        const title = req.body.title;
+        const newBook = new Book({ title });
+        newBook
+          .save()
+          .then(result => {
+            var { title, _id } = result;
+            res.status(200).json({ title, _id });
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete((req, res) => {
       //if successful response will be 'complete delete successful'
       Book.deleteMany({}, (err, del) => {
-        if(err) res.status(503).send('could not delete ');
-        res.status(200).send('complete delete successful');
-      })      
+        if (err) res.status(503).send("could not delete ");
+        res.status(200).send("complete delete successful");
+      });
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
-      var bookid = req.params.id;
+  app
+    .route("/api/books/:id")
+    .get((req, res) => {
+      const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-      Book.findById(bookid, '_id title comments', (err, doc) => {   
-        if(err) {
+      Book.findById(bookid, "_id title comments", (err, doc) => {
+        if (err) {
           console.log(err);
-        }else if(!doc){
-          res.status(500).send('no book exists');
-        }else{
+        } else if (!doc) {
+          res.status(500).send("no book exists");
+        } else {
           res.status(200).json(doc);
         }
-      })
+      });
     })
-    
-    .post(function(req, res){
-      var bookid = req.params.id;
-      var comment = req.body.comment;
-      Book.findByIdAndUpdate(bookid, {$push : {comments: comment}}, {useFindAndModify: false, select: '_id title comments', "new": true, "upsert": true}, (err, doc) => {
-        if(err) console.log(err);
-        res.status(200).json(doc);        
-      })
+
+    .post((req, res) => {
+      const bookid = req.params.id;
+      const comment = req.body.comment;
+      Book.findByIdAndUpdate(
+        bookid,
+        { $push: { comments: comment } },
+        {
+          useFindAndModify: false,
+          select: "_id title comments",
+          new: true,
+          upsert: true,
+        },
+        (err, doc) => {
+          if (err) console.log(err);
+          res.status(200).json(doc);
+        },
+      );
     })
-    
-    .delete(function(req, res){
-      var bookid = req.params.id;
+
+    .delete((req, res) => {
+      const bookid = req.params.id;
       //if successful response will be 'delete successful'
-      Book.deleteOne({_id: bookid}, (err, result) => {
-        if(err) res.status(500).send('error on delete');
-        res.status(200).send('delete successful')        
-      })
+      Book.deleteOne({ _id: bookid }, (err, result) => {
+        if (err) res.status(500).send("error on delete");
+        res.status(200).send("delete successful");
+      });
     });
-  
 };
--- server.js
+++ server.js
@@ -1,58 +1,59 @@
-'use strict';
-require('dotenv').config();
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var cors        = require('cors');
-var helmet = require('helmet');
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const apiRoutes = require("./routes/api.js");
+const fccTestingRoutes = require("./routes/fcctesting.js");
+const runner = require("./test-runner");
 
-var app = express();
+const app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use(cors({origin: '*'})); //USED FOR FCC TESTING PURPOSES ONLY!
+app.use(cors({ origin: "*" })); //USED FOR FCC TESTING PURPOSES ONLY!
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use( helmet( {
-  noCache       : true,
-  hidePoweredBy : { setTo: 'PHP 4.2.0' },
-  xssFilter     : true,
-} ) );
+app.use(
+  helmet({
+    noCache: true,
+    hidePoweredBy: { setTo: "PHP 4.2.0" },
+    xssFilter: true,
+  }),
+);
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
-    
+//Routing for API
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+  res
+    .status(404)
+    .type("text")
+    .send("Not Found");
 });
 
 //Start our server and tests!
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log("Tests are not valid:");
+        console.log(error);
       }
     }, 1500);
   }
